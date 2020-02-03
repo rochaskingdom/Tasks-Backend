2a3649e4ef7031a0e9b3b5fd57b437b486cd8cab
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-03 13:10:58 -0300

    DB Migrations

--- migrations/20200203124253_create_table_tasks.js
+++ migrations/20200203124253_create_table_tasks.js
@@ -1,6 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.scheme.createTable('tasks', table => {
+      table.increments('id').primary()
+      table.string('desc').notNull()
+      table.datetime('estimateAt')
+      table.datetime('doneAt')
+      table.integer('userId').references('id')
+        .inTable('users').notNull()
+  })
 };
 
 exports.down = function(knex) {
